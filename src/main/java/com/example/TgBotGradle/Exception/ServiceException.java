3a1ad30fe6a59fc520f1106fae771f82422aa500
Emotional-emptiness <moon.linexx@gmail.com>
2023-10-28 22:32:27 +0300

    TgBot Gradle

---
+++ src/main/java/com/example/TgBotGradle/Exception/ServiceException.java
@@ -0,0 +1,7 @@
+package com.example.TgBotGradle.Exception;
+
+public class ServiceException extends Exception{
+    public ServiceException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/example/TgBotGradle/TgBotGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.TgBotGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBotGradleApplication.class, args);
+	}
+
+}
